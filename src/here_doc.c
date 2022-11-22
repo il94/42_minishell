0e1ac16e6f3e5274de1b81181c296d9bfcbad343
ahmetuz <pro.ahmetuzn@gmail.com>
2022-11-23 00:20:36 +0100

    heredoc start

--- src/here_doc.c
+++ src/here_doc.c
@@ -3,51 +3,67 @@
 /*                                                        :::      ::::::::   */
 /*   here_doc.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ilandols <ilandols@student.42.fr>          +#+  +:+       +#+        */
+/*   By: auzun <auzun@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 06:12:24 by auzun             #+#    #+#             */
-/*   Updated: 2022/11/22 16:04:48 by ilandols         ###   ########.fr       */
+/*   Updated: 2022/11/22 23:51:07 by auzun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-static void	opening_fd_here_doc(t_data *data, t_fd *file, char *here_doc_name)
+void	sig_unexpected_eof(char *delimiter)
 {
-	file->fd = open(here_doc_name, O_RDWR | O_EXCL | O_CREAT, 0644);
-	if (file->fd == -1)
-		define_exit_status("minishell: error: cant create heredoc.\n", 1);
+	msg_error("minishell: ");
+	msg_error("warning: here-document delimited by end-of-file (wanted `");
+	msg_error(delimiter);
+	msg_error("')\n");
 }
 
-static void	writing_here_doc(t_data *data, t_fd *file)
+static void	writing_here_doc(t_data *data, t_fd *file, int fd)
 {
 	char	*buffer;
 
 	buffer = NULL;
 	while (1)
 	{
-		ft_printf("heredoc> ");
+		ft_printf("> ");
 		buffer = ft_get_next_line(STDIN_FILENO);
 		if (!buffer)
-			free_all_and_exit(data, "malloc");
+		{
+			sig_unexpected_eof(file->file);
+			return ;
+		}
 		buffer[ft_strlen(buffer) - 1] = '\0';
-		if (!ft_strcmp(av[2], buffer))
+		if (!ft_strcmp(file->file, buffer))
 			break ;
-		write(file->fd, buffer, ft_strlen(buffer));
+		write(fd, buffer, ft_strlen(buffer));
 		free(buffer);
 	}
 	free(buffer);
-	close(file->fd);
-	file->fd = -1;
+	close(fd);
 }
 
 void	generate_here_doc(t_data *data, t_fd *file)
 {
-	char	*here_doc_name;
+	pid_t	pid_here_doc;
+	int		fd_here_doc;
 
-	/*here_doc_name = random_name...*/
-	opening_fd_here_doc(data, fd);
-	if (g_exit_status)
-		return ;
-	writing_here_doc(cmd_list, fd_list, av);
+	if (pipe(fd_here_doc) == -1)
+		free_all_and_exit(data, "pipe");
+	file->fd = fd_here_doc[0];
+	pid = fork()
+	if (pid == -1)
+		free_all_and_exit(data, "pipe");
+	if (pid == 0)
+	{
+		writing_here_doc(cmd_list, fd_list, av);
+		free_data_struct(data);
+		close(fd_here_doc[1]);
+		exit (0);
+	}
+	wait(NULL);
+	close(fd_here_doc[1]);
+	return ;
 }
+
